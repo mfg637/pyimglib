c96141d995775b8a2560064ac79bab56d42a7557
mfg637 <mfg636@gmail.com>
2023-08-27 19:21:25 +0300

    trying to speedup batch transcoding

--- transcoding/encoders/webp_encoder.py
+++ transcoding/encoders/webp_encoder.py
@@ -10,13 +10,14 @@ MAX_SIZE = 16383
 
 class WEBPEncoder(BytesEncoder):
     SUFFIX = ".webp"
+    effort = 4
     def __init__(self, source, img: PIL.Image.Image):
         BytesEncoder.__init__(self, self.SUFFIX)
         self.source = img
 
     def encode(self, quality, lossless=False) -> bytes:
         lossy_out_io = io.BytesIO()
-        self.source.save(lossy_out_io, format="WEBP", lossless=lossless, quality=quality, method=6)
+        self.source.save(lossy_out_io, format="WEBP", lossless=lossless, quality=quality, method=self.effort)
         return lossy_out_io.getvalue()
 
 
