5f6e5b93f063c96f5871e1a379fed088209d5509
mfg637 <mfg636@gmail.com>
2025-07-29 23:46:44 +0300

    enhance extraction for IPTC metadata

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -10,7 +10,10 @@ def get_metadata_from_source(source, _format) -> dict[str, str]:
     if _format == "png":
         return png_reader.read(source)
     elif _format in {"jpg", "jpeg", "jfif", "webp"}:
-        return exif_reader.read(source)
+        metadata = exif_reader.read(source)
+        if _format in {"jpg", "jpeg", "jfif"}:
+            metadata.update(iptc_reader.read(source))
+        return metadata
     else:
         logger.warning(f"Not found reader for format: {_format}")
         return {}
--- metadata/iptc_reader.py
+++ metadata/iptc_reader.py
@@ -1,7 +1,6 @@
 import io
 import logging
 import iptcinfo3
-from struct import unpack
 
 logger = logging.getLogger(__name__)
 
@@ -10,59 +9,16 @@ def default_handler(value) -> str:
     if value is None:
         return ""
     elif type(value) is bytes:
-        return value.decode()
+        try:
+            return value.decode()
+        except UnicodeDecodeError:
+            return value.decode("latin-1", errors="replace")
     elif isinstance(value, list):
         return ", ".join(value)
     return str(value)
 
 
-def collectIIMInfo(fh):
-    """
-    Taken and modified from iptcinfo3. Used for png chunk metadata extraction.
-
-    Assuming file is seeked to start of IIM data (using above),
-    this reads all the data into our object's hashes"""
-    # NOTE: file should already be at the start of the first
-    # IPTC code: record 2, dataset 0.
-
-    _data = iptcinfo3.IPTCData(
-        {
-            "supplemental category": [],
-            "keywords": [],
-            "contact": [],
-        }
-    )
-
-    while True:
-        try:
-            header = iptcinfo3.read_exactly(fh, 5)
-        except iptcinfo3.EOFException:
-            return _data
-
-        (tag, record, dataset, length) = unpack("!BBBH", header)
-        # bail if we're past end of IIM record 2 data
-        if not (tag == 0x1C and record == 2):
-            return _data
-
-        alist = {
-            "tag": tag,
-            "record": record,
-            "dataset": dataset,
-            "length": length,
-        }
-        logger.debug("\t".join("%s: %s" % (k, v) for k, v in alist.items()))
-        value = fh.read(length)
-
-        # try to extract first into _listdata (keywords, categories)
-        # and, if unsuccessful, into _data. Tags which are not in the
-        # current IIM spec (version 4) are currently discarded.
-        if dataset in _data and hasattr(_data[dataset], "append"):
-            _data[dataset].append(value)
-        elif dataset != 0:
-            _data[dataset] = value
-
-
-def prepare_data(info_dict: iptcinfo3.IPTCInfo | iptcinfo3.IPTCData):
+def prepare_data(info_dict: iptcinfo3.IPTCData):
     extracted_info = dict()
     for key in info_dict:
         key_str = iptcinfo3.c_datasets.get(key, str(key))
@@ -80,9 +36,9 @@ def read_png_chunk(chunk_data: bytes) -> dict[str, str]:
     file_interface = io.BytesIO(
         bytes.fromhex(text_hex_sequence.replace("\n", ""))
     )
-    print("file_interface", file_interface.getvalue())
-    info_dict = collectIIMInfo(file_interface)
-    return prepare_data(info_dict)
+    info_object = iptcinfo3.IPTCInfo(io.BytesIO(b""))
+    info_object.collectIIMInfo(file_interface)
+    return prepare_data(info_object._data)
 
 
 def read(source) -> dict[str, str]:
@@ -91,4 +47,4 @@ def read(source) -> dict[str, str]:
         info_object = iptcinfo3.IPTCInfo(file_interface)
     else:
         info_object = iptcinfo3.IPTCInfo(str(source))
-    return prepare_data(info_object)
+    return prepare_data(info_object._data)
